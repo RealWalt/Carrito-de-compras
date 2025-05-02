3af54dcac399ba5cfd2c243a9f6aeae47d51d690
RealWalt <84933860+RealWalt@users.noreply.github.com>
2025-05-02 14:11:02 -0400

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Variables
+ // Variables
 const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
